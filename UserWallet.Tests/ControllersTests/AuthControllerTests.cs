071b68751d0fdbfc943c6928e8832bd4f4ba041e
Packword <maksim_zhelnin@mail.ru>
2023-08-04 08:32:50 +0300

    Refactor auth controller tests

--- UserWallet.Tests/ControllersTests/AuthControllerTests.cs
+++ UserWallet.Tests/ControllersTests/AuthControllerTests.cs
@@ -1,50 +1,7 @@
 namespace UserWallet.Tests.ControllersTests
 {
-    public class AuthTests
+    public class AuthTests: BaseControllerTest
     {
-        private WebApplicationFactory<Program> _factory;
-        private WebApplicationFactoryHelper _factoryHelper = new WebApplicationFactoryHelper();
-        private AuthServiceHelper _authServiceHelper;
-        private HttpClient _client;
-        private string initialDirectory;
-
-        [OneTimeSetUp]
-        public void OneTimeSetup()
-        {
-            initialDirectory = Directory.GetCurrentDirectory();
-
-            Directory.SetCurrentDirectory("../../../../UserWallet");
-            _factory = _factoryHelper.CreateFactoryWithInMemoryDb();
-
-        }
-
-        [SetUp]
-        public void Setup()
-        {
-            _client = _factory.CreateClient();
-            _authServiceHelper = new AuthServiceHelper(_client);
-            var sp = _factory.Services;
-            using var scope = sp.CreateScope();
-            var scopedServices = scope.ServiceProvider;
-            var db = scopedServices.GetRequiredService<ApplicationDbContext>();
-            db.Database.EnsureDeleted();
-            db.Database.EnsureCreated();
-            SeedDataFromJson.Initialize(scopedServices);
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            _client.Dispose();
-        }
-
-        [OneTimeTearDown]
-        public void OneTimeTearDown()
-        {
-            Directory.SetCurrentDirectory(initialDirectory);
-            _factory.Dispose();
-        }
-
         [Test]
         public async Task PositiveLoginAsAdminTest()
         {
@@ -61,16 +18,6 @@ namespace UserWallet.Tests.ControllersTests
 
         [Test]
         public async Task PositiveChangePasswordTest()
-        {
-            await _authServiceHelper.Login("Admin", "1234");
-            var requestMessage = new HttpRequestMessage(HttpMethod.Patch, "/auth/change-password");
-            requestMessage.Content = JsonContent.Create(new ChangeUserPasswordDTO { NewPassword = "12345" });
-            var response = await _client.SendAsync(requestMessage);
-            response.StatusCode.Should().Be(HttpStatusCode.OK);
-        }
-
-        [Test]
-        public async Task LoginAfterChangePasswordTest()
         {
             await _authServiceHelper.Login("Admin", "1234");
             var requestMessage = new HttpRequestMessage(HttpMethod.Patch, "/auth/change-password");
