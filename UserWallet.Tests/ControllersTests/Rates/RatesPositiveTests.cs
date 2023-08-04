9a085453ad2997c09be9bb3f5670f04962da4b4e
Packword <maksim_zhelnin@mail.ru>
2023-08-04 14:52:23 +0300

    Added rates controller positive tests

---
+++ UserWallet.Tests/ControllersTests/Rates/RatesPositiveTests.cs
@@ -0,0 +1,19 @@
+﻿namespace UserWallet.Tests.ControllersTests.Rates
+{
+    public class RatesPositiveTests: BaseControllerTest
+    {
+        [Test]
+        public async Task PositiveGetRatesControllerTest()
+        {
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, $"/rates");
+            var response = await _client.SendAsync(requestMessage);
+            var content = await response.Content.ReadAsStringAsync();
+            var rates = JsonSerializer.Deserialize<CurrentRateDTO>(content, TestData.JSON_OPTIONS);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            rates!.Rates.ContainsKey("btc").Should().Be(true);
+            rates.Rates.ContainsKey("rub").Should().Be(true);
+            rates.Rates.ContainsKey("usd").Should().Be(false);
+        }
+    }
+}
