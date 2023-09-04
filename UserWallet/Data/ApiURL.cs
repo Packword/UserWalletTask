93910d34d9976322da12cfe48f8eb070608a374e
Packword <maksim_zhelnin@mail.ru>
2023-09-04 12:06:46 +0300

    Created static classes to save const data(URL, JsonSerializerOptions) and placed the client in using statement

---
+++ UserWallet/Data/ApiURL.cs
@@ -0,0 +1,8 @@
+﻿namespace UserWallet.Data
+{
+    public static class ApiURL
+    {
+        public const string LOGIN_URL = @"http://localhost:5140/auth/login";
+        public const string LOGOUT_URL = @"http://localhost:5140/auth/logout";
+    }
+}
---
+++ UserWallet/Data/BlazorOptions.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json;
+
+namespace UserWallet.Data
+{
+    public static class BlazorOptions
+    {
+        public static readonly JsonSerializerOptions JSON_SERIALIZE_OPTIONS = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+    }
+}
